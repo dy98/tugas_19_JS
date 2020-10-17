632cfec2137b76aec598282ab962234810093369
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 15:46:50 +0700

    tugas 19 JS
    tugas untuk Niomic

---
+++ tugas19-1.js
@@ -0,0 +1,6 @@
+function repeat() {
+    let semangat = 'Semangat...';
+
+    console.log(semangat.repeat(5));
+}
+repeat();
---
+++ tugas19-2.js
@@ -0,0 +1,14 @@
+function repeat() {
+    let bintang = '*';
+
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(5 * 5));
+    console.log(bintang.repeat(10 * 5));
+    console.log(bintang.repeat(15 * 5));
+    console.log(bintang.repeat(20 * 5));
+    console.log(bintang.repeat(15 * 5));
+    console.log(bintang.repeat(10 * 5));
+    console.log(bintang.repeat(5 * 5));
+    console.log(bintang.repeat(5));
+}
+repeat();
